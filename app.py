bd562461e2cc5a894df5d135cec303ed8a629f93
aambrioso <aambrioso1@gmail.com>
2020-12-13 15:16:00 -0500

    Initial files for my_blog

--- app.py
+++ app.py
@@ -3,11 +3,14 @@ from sqlalchemy import create_engine, MetaData
 from flask_login import UserMixin, LoginManager, login_user, logout_user
 from flask_blogging import SQLAStorage, BloggingEngine
 
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret"  # for WTF-forms and login
 app.config["BLOGGING_URL_PREFIX"] = "/blog"
 app.config["BLOGGING_DISQUS_SITENAME"] = "test"
-app.config["BLOGGING_SITEURL"] = "http://localhost:8000"
+app.config["BLOGGING_SITEURL"] = "http://localhost:5000"
 app.config["BLOGGING_SITENAME"] = "My Site"
 app.config["BLOGGING_KEYWORDS"] = ["blog", "meta", "keywords"]
 app.config["FILEUPLOAD_IMG_FOLDER"] = "fileupload"
@@ -15,7 +18,8 @@ app.config["FILEUPLOAD_PREFIX"] = "/fileupload"
 app.config["FILEUPLOAD_ALLOWED_EXTENSIONS"] = ["png", "jpg", "jpeg", "gif"]
 
 # extensions
-engine = create_engine('sqlite:////tmp/blog.db')
+db_file = 'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+engine = create_engine(db_file)
 meta = MetaData()
 sql_storage = SQLAStorage(engine, metadata=meta)
 blog_engine = BloggingEngine(app, sql_storage)
@@ -70,4 +74,4 @@ def logout():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8000, use_reloader=True)
+    app.run(debug=True, port=5000, use_reloader=True)
