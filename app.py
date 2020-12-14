a6173231b3c4b72c767a1428c9c3c140d86db107
aambrioso <aambrioso1@gmail.com>
2020-12-13 20:50:35 -0500

    modify app.py and service

--- app.py
+++ app.py
@@ -72,6 +72,8 @@ def logout():
     logout_user()
     return redirect("/")
 
+def main():
+    app.run(debug=True, port=5000)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000, use_reloader=True)
