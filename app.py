0c9a4fc18eb02ea3783f7d326ab1737f9dc38341
aambrioso <aambrioso1@gmail.com>
2021-01-23 18:25:08 -0500

    fix image folder problem

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from flask_blogging import SQLAStorage, BloggingEngine
 
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
+image_folder = os.path.join('static', 'img')
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret"  # for WTF-forms and login
@@ -22,6 +23,7 @@ app.config["BLOGGING_KEYWORDS"] = ["blog", "meta", "keywords"]
 app.config["FILEUPLOAD_IMG_FOLDER"] = "fileupload"
 app.config["FILEUPLOAD_PREFIX"] = "/fileupload"
 app.config["FILEUPLOAD_ALLOWED_EXTENSIONS"] = ["png", "jpg", "jpeg", "gif"]
+app.config['image_folder'] = image_folder
 
 # This configuration will make the app less hackable.
 app.config["ADMIN_PASSWORD"] = os.environ.get("ADMIN_PASSWORD")
@@ -162,7 +164,8 @@ def fake_login():
 
 @app.route("/mom/")
 def mom():
-    return render_template('mom.html')
+    full_filename=os.path.join(app.config['image_folder'])
+    return render_template('mom.html', user_image = full_filename)
 
 # This main function is needed to get wsgi to work properly with my deployment method.
 # Need confirm this fact.
